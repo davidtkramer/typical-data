e769008233d1b599cc36bd778e35e138d5710d69
David Kramer <david.kramer@redtailtechnology.com>
2021-07-25 16:06:39 -0700

    basic functionality for new factory

--- src/__tests__/factory.new.test.ts
+++ src/__tests__/factory.new.test.ts
@@ -1,127 +1,376 @@
-import { Factory } from '../factory.new';
+import { EntityFactory, Factory } from '../factory.new';
 
-interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  isAdmin: boolean;
-  isActive: boolean;
-}
+/**
+ * TODO
+ * - implement buildList
+ * - runtime warnings for invalid dsl invocations
+ * - omit trait from builder returned by afterCreate
+ */
 
-const userFactory = Factory.define((factory) =>
-  factory
-    .transient({
-      baz: true,
-    })
-    .attributes<User>({
-      id: 1,
-      firstName: 'Alice',
-      lastName: 'Smith',
-      isAdmin: false,
-      isActive({ sequence, params, transientParams }) {
-        return true;
-      },
-    })
-    .trait('admin', (trait) =>
-      trait
-        .transient({
-          foo: true,
-        })
-        .attributes({
-          isAdmin({ sequence, params, transientParams }) {
-            return true;
-          },
-        })
-        .afterCreate((entity, { transientParams }) => {})
-    )
-    .trait('standard', (trait) =>
-      trait
-        .transient({
-          bar: 'bar',
-        })
-        .attributes({
-          isAdmin: true,
-        })
-        .afterCreate((entity, { transientParams }) => {})
-    )
-    .trait('inactive', (trait) =>
-      trait.attributes({
-        isActive: false,
+describe('build', () => {
+  const factory = Factory.define((factory) =>
+    factory
+      .transient({
+        skipGlobalHook: false,
       })
-    )
-    .afterCreate((entity, { transientParams }) => {})
-);
-
-describe('Factory', () => {
-  describe('build', () => {
-    it('builds entity with no attribute overrides', () => {
-      userFactory.build();
-    });
-
-    it('builds entity with attribute overrides', () => {
-      userFactory.build({ id: 1 });
-    });
-
-    it('builds entity with attribute overrides and global transient params', () => {
-      userFactory.build({ id: 1, baz: true });
-    });
-
-    it('builds entity with single trait', () => {
-      userFactory.build('admin');
-      userFactory.build('inactive');
-    });
-
-    it('builds entity with single trait and attribute overrides', () => {
-      userFactory.build('inactive', { id: 1 });
-    });
-
-    it('builds entity with single trait, trait transient params, and attribute overrides', () => {
-      userFactory.build('admin', { id: 1, foo: true });
-    });
-
-    it('builds entity with single trait, global transient params, and attribute overrides ', () => {
-      userFactory.build('admin', { id: 1, baz: true });
-    });
-
-    it('builds entity with multiple traits', () => {
-      userFactory.build('admin', 'inactive');
-    });
-
-    it('builds entity with multiple traits, one trait transient param, and attribute overrides', () => {
-      userFactory.build('admin', 'inactive', { id: 1, foo: true });
-    });
-
-    it('builds entity with multiple traits, multiple trait transient params, and attribute overrides', () => {
-      userFactory.build('admin', 'inactive', 'standard', {
+      .attributes<{
+        id: number;
+        type: 'standard' | 'bot';
+        role: 'member' | 'admin' | 'owner';
+        name: string;
+        lastHook: string;
+      }>({
         id: 1,
-        foo: true,
-        bar: '',
-      });
+        type: 'standard',
+        role: 'member',
+        name: 'Alice',
+        lastHook: '',
+      })
+      .trait('admin', (trait) =>
+        trait.attributes({
+          role: 'admin',
+        })
+      )
+      .trait('owner', (trait) =>
+        trait
+          .transient({ skipOwnerHook: false })
+          .attributes({
+            role: 'owner',
+          })
+          .afterCreate((entity, { transientParams }) => {
+            if (transientParams.skipOwnerHook) return;
+            entity.lastHook = 'owner';
+          })
+      )
+      .trait('bot', (trait) =>
+        trait
+          .transient({ skipBotHook: false })
+          .attributes({
+            type: 'bot',
+          })
+          .afterCreate((entity, { transientParams }) => {
+            if (transientParams.skipBotHook) return;
+            entity.lastHook = 'bot';
+          })
+      )
+      .afterCreate((entity, { transientParams }) => {
+        if (transientParams.skipGlobalHook) return;
+        entity.lastHook = 'global';
+      })
+  );
+
+  beforeEach(() => {
+    factory.rewindSequence();
+  });
+
+  it('builds default entity', () => {
+    const user = factory.build();
+    expect(user).toEqual({
+      id: 1,
+      type: 'standard',
+      role: 'member',
+      name: 'Alice',
+      lastHook: 'global',
     });
+  });
 
-    it('increments sequence for each invocation', () => {});
-
-    // type tests
-
-    it('rejects trait transient params when no traits are provided', () => {
-      // @ts-expect-error - foo is not available
-      userFactory.build({ foo: true });
+  it('builds entity with attribute overrides', () => {
+    const user = factory.build({
+      id: 10,
+      type: 'bot',
+      role: 'admin',
+      name: 'bot',
     });
-
-    it('rejects trait transient params when corresponding trait is absent', () => {
-      // @ts-expect-error - foo is not available
-      userFactory.build('inactive', { foo: true });
-      // @ts-expect-error - bar is not available
-      userFactory.build('admin', 'inactive', { foo: true, bar: '' });
-      // @ts-expect-error - bar is not available
-      userFactory.build('admin', { foo: true, bar: '' });
+    expect(user).toEqual({
+      id: 10,
+      type: 'bot',
+      role: 'admin',
+      name: 'bot',
+      lastHook: 'global',
     });
+  });
+
+  it('builds entity with single trait', () => {
+    const user = factory.build('admin');
+    expect(user.role).toBe('admin');
+  });
+
+  it('builds entity with single trait and attribute overrides', () => {
+    const user = factory.build('admin', { id: 10 });
+    expect(user.role).toBe('admin');
+    expect(user.id).toBe(10);
+  });
+
+  it('builds entity with single trait, transient params, and attribute overrides', () => {
+    const user = factory.build('owner', {
+      id: 10,
+      skipOwnerHook: true,
+      skipGlobalHook: true,
+    });
+    expect(user.id).toBe(10);
+    expect(user.role).toBe('owner');
+    expect(user.lastHook).toBe('');
+  });
+
+  it('gives precedence to attribute overrides over trait attributes', () => {
+    const user = factory.build('admin', { role: 'owner' });
+    expect(user.role).toBe('owner');
+  });
+
+  it('builds entity with multiple traits', () => {
+    const user = factory.build('admin', 'bot');
+    expect(user.role).toBe('admin');
+    expect(user.type).toBe('bot');
+  });
+
+  it('builds entity with multiple traits and overrides', () => {
+    const user = factory.build('admin', 'bot', { id: 10 });
+    expect(user.role).toBe('admin');
+    expect(user.type).toBe('bot');
+    expect(user.id).toBe(10);
+  });
+
+  it('builds entity with multiple traits, transient params, and attribute overrides', () => {
+    const user = factory.build('owner', 'admin', 'bot', {
+      id: 10,
+      skipBotHook: true,
+      skipOwnerHook: true,
+      skipGlobalHook: true,
+    });
+    expect(user.id).toBe(10);
+    expect(user.role).toBe('admin');
+    expect(user.type).toBe('bot');
+    expect(user.lastHook).toBe('');
+  });
+
+  it('gives precedence to rightmost traits', () => {
+    const ownerUser = factory.build('admin', 'owner');
+    expect(ownerUser.role).toBe('owner');
+  });
+
+  it('runs global afterCreate hooks', () => {
+    const user = factory.build();
+    expect(user.lastHook).toBe('global');
+  });
+
+  it('runs global afterCreate hooks after trait afterCreate hooks', () => {
+    const user = factory.build('owner', 'bot');
+    expect(user.lastHook).toBe('global');
+  });
+
+  it('runs trait afterCreate hooks in same order as provided traits', () => {
+    const user1 = factory.build('owner', 'bot', { skipGlobalHook: true });
+    expect(user1.lastHook).toBe('bot');
+
+    const user2 = factory.build('bot', 'owner', { skipGlobalHook: true });
+    expect(user2.lastHook).toBe('owner');
+  });
+
+  it('rejects trait transient params when no traits are provided', () => {
+    // @ts-expect-error - skipOwnerHook is not available
+    factory.build({ skipOwnerHook: true });
+  });
+
+  it('rejects trait transient params when corresponding trait is absent', () => {
+    // @ts-expect-error - skipOwnerHook is not available
+    factory.build('admin', { skipOwnerHook: true });
+    // @ts-expect-error - skipOwnerHook is not available
+    factory.build('admin', 'bot', { skipBotHook: true, skipOwnerHook: true });
+    // @ts-expect-error - skipOwnerHook is not available
+    factory.build('bot', { skipBotHook: true, skipOwnerHoook: true });
   });
 });
 
-describe('factory DSL', () => {
-  describe('transient', () => {});
-  describe('attributes', () => {});
-  describe('trait', () => {});
-  describe('afterCreate', () => {});
+describe('DSL', () => {
+  describe('transient', () => {
+    it('can handle optional params', () => {
+      type TransientParams = { globalTransientId?: number };
+      const factory = Factory.define((factory) =>
+        factory.transient<TransientParams>({}).attributes<{ id: number }>({
+          id: ({ transientParams }) => transientParams.globalTransientId ?? 1,
+        })
+      );
+
+      // default
+      const user1 = factory.build();
+      expect(user1.id).toBe(1);
+
+      // override
+      const user2 = factory.build({ globalTransientId: 10 });
+      expect(user2.id).toBe(10);
+    });
+  });
+
+  describe('attributes', () => {
+    it('can use sequence in attribute builder', () => {
+      const factory = Factory.define((factory) =>
+        factory.attributes<{ id: number }>({
+          id: ({ sequence }) => sequence + 1,
+        })
+      );
+
+      const user1 = factory.build();
+      const user2 = factory.build();
+      const user3 = factory.build();
+      expect(user1.id).toBe(1);
+      expect(user2.id).toBe(2);
+      expect(user3.id).toBe(3);
+    });
+
+    it('can use global transient params in attribute builder', () => {
+      const factory = Factory.define((factory) =>
+        factory
+          .transient({
+            globalTransientId: 10,
+          })
+          .attributes<{ id: number }>({
+            id: ({ transientParams }) => transientParams.globalTransientId,
+          })
+      );
+
+      // default
+      const user1 = factory.build({ globalTransientId: 10 });
+      expect(user1.id).toBe(10);
+
+      // override
+      const user2 = factory.build({ globalTransientId: 20 });
+      expect(user2.id).toBe(20);
+    });
+
+    it('can use params in attribute builder', () => {
+      const factory = Factory.define((factory) =>
+        factory.attributes<{ id: number; name: string }>({
+          id: 1,
+          name({ params }) {
+            return (params?.id ?? 0) % 2 === 0 ? 'even' : 'odd';
+          },
+        })
+      );
+
+      const user1 = factory.build({ id: 10 });
+      expect(user1.name).toBe('even');
+
+      const user2 = factory.build({ id: 11 });
+      expect(user2.name).toBe('odd');
+    });
+  });
+
+  describe('traits', () => {
+    it('can use sequence in attribute builder', () => {
+      const factory = Factory.define((factory) =>
+        factory
+          .attributes<{ id: number }>({
+            id: 1,
+          })
+          .trait('sequence', (trait) =>
+            trait.attributes({
+              id: ({ sequence }) => sequence + 1,
+            })
+          )
+      );
+
+      const user1 = factory.build('sequence');
+      const user2 = factory.build('sequence');
+      expect(user1.id).toBe(1);
+      expect(user2.id).toBe(2);
+    });
+
+    it('can use transient params in attribute builder', () => {
+      const factory = Factory.define((factory) =>
+        factory
+          .transient({ globalTransientId: 10 })
+          .attributes<{ id: number; name: string }>({
+            id: 1,
+            name: 'name',
+          })
+          .trait('transient', (trait) =>
+            trait
+              .transient({ traitTransientName: 'transientNameDefault' })
+              .attributes({
+                id: ({ transientParams }) => transientParams.globalTransientId,
+                name: ({ transientParams }) =>
+                  transientParams.traitTransientName,
+              })
+          )
+      );
+
+      // default
+      const user1 = factory.build('transient');
+      expect(user1.id).toBe(10);
+      expect(user1.name).toBe('transientNameDefault');
+
+      // override
+      const user2 = factory.build('transient', {
+        globalTransientId: 20,
+        traitTransientName: 'transientNameOverride',
+      });
+      expect(user2.id).toBe(20);
+      expect(user2.name).toBe('transientNameOverride');
+    });
+
+    it('can use params in attribute builder', () => {
+      const factory = Factory.define((factory) =>
+        factory
+          .attributes<{ id: number; name: string }>({ id: 1, name: 'name' })
+          .trait('evenOdd', (trait) =>
+            trait.attributes({
+              name({ params }) {
+                return (params?.id ?? 0) % 2 === 0 ? 'even' : 'odd';
+              },
+            })
+          )
+      );
+
+      const user1 = factory.build('evenOdd', { id: 10 });
+      expect(user1.name).toBe('even');
+
+      const user2 = factory.build('evenOdd', { id: 11 });
+      expect(user2.name).toBe('odd');
+    });
+
+    it('can use transientParams in afterCreate', () => {
+      const factory = Factory.define((factory) =>
+        factory
+          .transient({ globalTransientId: 10 })
+          .attributes<{ id: number; name: string }>({ id: 1, name: 'name' })
+          .trait('transient', (trait) =>
+            trait
+              .transient({ traitTransientName: 'transientNameDefault' })
+              .afterCreate((entity, { transientParams }) => {
+                entity.id = transientParams.globalTransientId;
+                entity.name = transientParams.traitTransientName;
+              })
+          )
+      );
+
+      // default
+      const user1 = factory.build('transient');
+      expect(user1.id).toBe(10);
+      expect(user1.name).toBe('transientNameDefault');
+
+      // override
+      const user2 = factory.build('transient', {
+        globalTransientId: 20,
+        traitTransientName: 'transientNameOverride',
+      });
+      expect(user2.id).toBe(20);
+      expect(user2.name).toBe('transientNameOverride');
+    });
+  });
+
+  describe('afterCreate', () => {
+    it('can use transientParams', () => {
+      const factory = Factory.define((factory) =>
+        factory
+          .transient({ globalTransientId: 10 })
+          .attributes<{ id: number }>({ id: 1 })
+          .afterCreate((entity, { transientParams }) => {
+            entity.id = transientParams.globalTransientId;
+          })
+      );
+
+      const user = factory.build({ globalTransientId: 20 });
+      expect(user.id).toBe(20);
+    });
+  });
 });
--- src/factory.new.ts
+++ src/factory.new.ts
@@ -18,7 +18,7 @@ type AttributeBuilder<
 type AttributeBuilderParams<Entity, TransientParams> = {
   sequence: number;
   params: Partial<Entity>;
-  transientParams?: TransientParams;
+  transientParams: TransientParams;
 };
 
 type TransientParamsForTraits<
@@ -99,8 +99,8 @@ interface FactoryBuilder<
   ): FinalBuilder<FactoryBuilder<Entity, GlobalTransientParams, Traits>>;
 }
 
-interface EntityFactory<Entity, GlobalTransientParams, Traits> {
-  build(entity?: Partial<Entity & GlobalTransientParams>): void;
+export interface EntityFactory<Entity, GlobalTransientParams, Traits> {
+  build(entity?: Partial<Entity & GlobalTransientParams>): Entity;
 
   build<TraitNames extends keyof Traits>(
     ...params:
@@ -113,7 +113,9 @@ interface EntityFactory<Entity, GlobalTransientParams, Traits> {
           >
         ]
       | Array<keyof Traits>
-  ): void;
+  ): Entity;
+
+  rewindSequence(): void;
 }
 
 export const Factory = {
@@ -122,26 +124,120 @@ export const Factory = {
       builder: FactoryBuilder
     ) => FinalBuilder<FactoryBuilder<Entity, GlobalTransientParams, Traits>>
   ): EntityFactory<Entity, GlobalTransientParams, Traits> {
+    let attributeDefaults: Record<string, any> = {};
+    let transientParamDefaults: Record<string, any> = {};
+    let traits: Record<string, any> = {};
+    let afterCreate: (...args: any[]) => void = () => null;
+    let sequence = -1;
+
     const factory: EntityFactory<Entity, GlobalTransientParams, Traits> = {
-      build(...params: Array<any>) {},
+      build(...args: any[]) {
+        let [params, ...traitNames] = args.reverse();
+        if (typeof params === 'string') {
+          traitNames.unshift(params);
+          params = {};
+        }
+
+        // split params into attributes and transient params
+        let attributes: Record<string, any> = {};
+        let transientParams: Record<string, any> = {
+          ...transientParamDefaults,
+        };
+        for (let key in params) {
+          if (key in attributeDefaults) {
+            attributes[key] = params[key];
+          } else {
+            transientParams[key] = params[key];
+          }
+        }
+
+        // build entity
+        const entity: Record<string, any> = attributes;
+        sequence++;
+
+        // apply defaults from traits
+        for (let traitName of traitNames) {
+          const trait = traits[traitName];
+
+          for (let key in trait.attributeDefaults) {
+            if (key in entity) continue;
+            let attribute = trait.attributeDefaults[key];
+            if (typeof attribute === 'function') {
+              entity[key] = attribute({
+                sequence,
+                params: { ...attributes, ...entity },
+                transientParams: {
+                  ...trait.transientParamDefaults,
+                  ...transientParams,
+                },
+              });
+            } else {
+              entity[key] = attribute;
+            }
+          }
+        }
+
+        // apply global defaults
+        for (let key in attributeDefaults) {
+          if (key in entity) continue;
+          let attribute = attributeDefaults[key];
+          if (typeof attribute === 'function') {
+            entity[key] = attribute({
+              sequence,
+              params: { ...attributes, ...entity },
+              transientParams,
+            });
+          } else {
+            entity[key] = attribute;
+          }
+        }
+
+        // after create hooks
+        for (let traitName of traitNames.reverse()) {
+          const trait = traits[traitName];
+          trait.afterCreate?.(entity, {
+            transientParams: {
+              ...trait.transientParamDefaults,
+              ...transientParams,
+            },
+          });
+        }
+        afterCreate(entity, { transientParams });
+
+        return entity as Entity;
+      },
+
+      rewindSequence() {
+        sequence = -1;
+      },
     };
 
     const factoryBuilder: FactoryBuilder<any, any, any> = {
-      attributes(attributes) {
+      attributes(params) {
+        attributeDefaults = params;
         return factoryBuilder;
       },
       transient(params) {
+        transientParamDefaults = params;
         return factoryBuilder;
       },
       trait(name, traitBuilderCallBack) {
+        traits[name] = {
+          attributeDefaults: {},
+          transientParamDefaults: {},
+          afterCreate: undefined,
+        };
         const traitBuilder: TraitBuilder<any, any, any> = {
           attributes(attributes) {
+            traits[name].attributeDefaults = attributes;
             return traitBuilder;
           },
           transient(params) {
+            traits[name].transientParamDefaults = params;
             return traitBuilder;
           },
           afterCreate(afterCreateCallback) {
+            traits[name].afterCreate = afterCreateCallback;
             return traitBuilder;
           },
         };
@@ -149,6 +245,7 @@ export const Factory = {
         return factoryBuilder;
       },
       afterCreate(afterCreateCallback) {
+        afterCreate = afterCreateCallback;
         return factoryBuilder;
       },
     };
